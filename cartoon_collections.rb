03594dbf72083844a8bf9b85425ea47d9603e66a
William Lin <willlin1996@gmail.com>
2020-03-05 02:59:39 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+dwarves.each.with_index do |dwarf, i| 
+puts "#{i + 1}. #{dwarf}"
+end 
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(object)
   # Your code here
+  object.collect { |wow| "#{wow.capitalize}" + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  if array.all? { |x| x.length <= 4}
+  false
+  elsif array.any? { |x| x.length >= 4 }
+  true
+else 
+  nil
+end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |x| x == cheese_types[0] || x == cheese_types[1] || x == cheese_types[2] }
 end
