11aaa6db5e9876bdf615ebcadab5620d32fbd891
William Lin <willlin1996@gmail.com>
2020-03-05 02:53:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(object)
   object.collect { |wow| "#{wow.capitalize}" + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 
